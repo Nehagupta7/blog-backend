40b9e314c9bfea5afe386ff6fb5d343feb78c1a0
Nehagupta7 <guptaneha4578@gmail.com>
2023-04-30 14:24:28 +0530

    update mongoose

--- config/db.js
+++ config/db.js
@@ -2,6 +2,7 @@ const mongoose=require("mongoose")
 console.log(process.env.PORT);
 const connectedDB=()=>{
     try{
+        mongoose.set("strictQuery", false);
 mongoose.connect("mongodb+srv://admin:liSULS6Nw2eWYu79@cluster0.jx35bo1.mongodb.net/Blog?retryWrites=true&w=majority")
 console.log("mongoDB connected")
     }catch(err){
